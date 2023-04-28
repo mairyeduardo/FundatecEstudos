bbac6c3f201f253a08fba27096708a298e5c7ad3
mairyeduardo <mairyeduardo@gmail.com>
2023-04-28 20:55:03 -0300

    alteracoes de melhoria do codigo

--- Segundo_Modulo/DesenvolvimentoWEB_II/NodeExercicios/exercicio0.2.2.js
+++ Segundo_Modulo/DesenvolvimentoWEB_II/NodeExercicios/exercicio0.2.2.js
@@ -1,28 +1,70 @@
 const usuario = {
     nome: "lucas",
-    idade: 22
+    idade: 22,
+    resposta: [
+        {
+            descricao: "Você possui que tipos de animais em sua casa?",
+            valores: ["cachorro", "gato"]
+        },
+        {
+            descricao: "Qual a profissão dos seus sonhos?",
+            valor: "bombeiro"
+        },
+        {
+            descricao: "O que você pensa do atual governo do pais?",
+            valor: "Muita coisa"
+        }
+
+    ]
 }
 
 const questionarios = [
     {
-        titulo: "Questionario Adoção de animais",
+        idTitulo: "Questionario Adoção de animais",
         tipo: "checkbox",
         pergunta: "Você possui que tipos de animais em sua casa?",
-        opcoes: ["cachorro", "tartaruga", "gato", "passarinho"],
-        resposta: ["cachorro", "gato"]
+        opcoes: [
+            {
+                id: '1',
+                descricao: "cachorro"
+            },
+            {
+                id: '2',
+                descricao: "tartaruga"
+            },
+            {
+                id: '3',
+                descricao: "gato"
+            },
+            {
+                id: '4',
+                descricao: "passarinho"
+            }
+        ],
     },
     {
-        titulo: "Questionario Objetivo do futuro",
+        idTitulo: "Questionario Objetivo do futuro",
         tipo: "radio",
         pergunta: "Qual a profissão dos seus sonhos?",
-        opcoes: ["bombeiro", "policial", "medico"],
-        resposta: ["bombeiro"]
+        opcoes: [
+            {
+                id: '1',
+                descricao: 'bombeiro'
+            },
+            {
+                id: '2',
+                descricao: "policial"
+            },
+            {
+                id: '3',
+                descricao: "medico"
+            }
+        ],
     },
     {
-        titulo: "Questionario Politico",
+        idTitulo: "Questionario Politico",
         tipo: "texto",
         pergunta: "O que você pensa do atual governo do pais?",
-        resposta: "Muita coisa"
     }
 
 ];
@@ -31,7 +73,6 @@ function buscartipo(buscar) {
     return questionarios.filter((questionario) => questionario.tipo === buscar);
 }
 
-console.log('Nome = ' + usuario.nome + ', Idade = ' + usuario.idade)
-
+console.log(JSON.stringify(usuario, null, Infinity))
 const questionario = buscartipo("radio")
-console.log(questionario);
+console.log(JSON.stringify(questionario, null, Infinity)); 
