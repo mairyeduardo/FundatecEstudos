7b8265852846a10d7f441bc867f69a9101cb4608
mairyeduardo <mairyeduardo@gmail.com>
2023-04-28 11:19:35 -0300

    exercicio finalizado

--- Segundo_Modulo/DesenvolvimentoWEB_II/NodeExercicios/exercicio0.1.0.js
+++ Segundo_Modulo/DesenvolvimentoWEB_II/NodeExercicios/exercicio0.1.0.js
@@ -1,17 +1,17 @@
-function cliente(valor, convenio, cartao){
-    if (cartao && convenio){
-        valor = (valor - (valor * 0.15));
+function cliente(valor, convenio, cartao) {
+    if (cartao && convenio) {
+        let desconto = valor * 0.15
         console.log('você possui desconto de 15%')
-        return valor;
-    } else if (convenio || cartao){
-        valor = (valor - (valor * 0.10));
+        return valor - desconto;
+    } else if (convenio || cartao) {
+        let desconto = valor * 0.10
         console.log('você possui desconto de 10%')
-        return valor;
+        return valor - desconto;
     } else {
         console.log('você não possui desconto')
         return valor;
     }
 }
 
-    const teste = cliente(20, true, false);
-    console.log(teste);
+const teste = cliente(20, true, false);
+console.log(teste);
