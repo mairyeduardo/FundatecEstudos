18cb1fe770e758a4c3676c81920a931605f36d96
mairyeduardo <mairyeduardo@gmail.com>
2023-04-28 11:24:54 -0300

    exercicio finalizado

--- Segundo_Modulo/DesenvolvimentoWEB_II/NodeExercicios/exercicio0.1.1.js
+++ Segundo_Modulo/DesenvolvimentoWEB_II/NodeExercicios/exercicio0.1.1.js
@@ -2,8 +2,8 @@ const anos = ['2012', '2010', '2020', '2022', '2035', '2045']
 
 function filtrarAnosMenores(ano) {
     let array = [];
-    for(let i = 0; i<anos.length; i++){
-        if(Number(anos[i]) < Number(ano)){ 
+    for (let i = 0; i < anos.length; i++) {
+        if (Number(anos[i]) < Number(ano)) {
             array.push(anos[i]);
         }
     }
@@ -14,8 +14,8 @@ console.log(filtrarAnosMenores(2020));
 
 function filtrarAnosMaiores(ano) {
     let array = [];
-    for(let i = 0; i<anos.length; i++){
-        if(Number(anos[i]) >= Number(ano)){ 
+    for (let i = 0; i < anos.length; i++) {
+        if (Number(anos[i]) >= Number(ano)) {
             array.push(anos[i]);
         }
     }
@@ -26,8 +26,8 @@ console.log(filtrarAnosMaiores(2022));
 
 function filtrarDivisiveis(numero = 10) {
     let array = [];
-    for(let i = 0; i<anos.length; i++){
-        if(Number(anos[i]) % Number(numero) === 0){ 
+    for (let i = 0; i < anos.length; i++) {
+        if (Number(anos[i]) % Number(numero) === 0) {
             array.push(anos[i]);
         }
     }
--- Segundo_Modulo/DesenvolvimentoWEB_II/NodeExercicios/exercicio0.1.2.js
+++ Segundo_Modulo/DesenvolvimentoWEB_II/NodeExercicios/exercicio0.1.2.js
@@ -1,17 +1,17 @@
-function limiteDesc(desc){
-    if(desc > 100){
+function limiteDesc(desc) {
+    if (desc > 100) {
         desc = 100;
     }
     return desc;
 }
 
-function cliente(valor, convenio, cartao){
-    if (cartao && convenio){
+function cliente(valor, convenio, cartao) {
+    if (cartao && convenio) {
         let desc = (valor * 0.15);
         valor = (valor - limiteDesc(desc));
         console.log('você possui desconto de 15% limitado a 100 reais!!!')
         return valor;
-    } else if (convenio || cartao){
+    } else if (convenio || cartao) {
         let desc = (valor * 0.10);
         valor = (valor - limiteDesc(desc));
         console.log('você possui desconto de 10% limitado a 100 reais!!!')
@@ -21,5 +21,5 @@ function cliente(valor, convenio, cartao){
         return valor;
     }
 }
-    const teste = cliente(3000, true, false);
-    console.log(teste);
+const teste = cliente(3000, true, false);
+console.log(teste);
