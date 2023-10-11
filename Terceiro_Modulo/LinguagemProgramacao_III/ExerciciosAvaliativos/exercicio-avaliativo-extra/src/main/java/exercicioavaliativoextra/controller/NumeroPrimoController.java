9a293cdb90bd27005f1815885177d1249aafed3b
mairyeduardo <mairyeduardo@gmail.com>
2023-10-10 22:21:55 -0300

    numeroPrimo Service e Controller

--- Terceiro_Modulo/LinguagemProgramacao_III/ExerciciosAvaliativos/exercicio-avaliativo-extra/src/main/java/exercicioavaliativoextra/controller/NumeroPrimoController.java
+++ Terceiro_Modulo/LinguagemProgramacao_III/ExerciciosAvaliativos/exercicio-avaliativo-extra/src/main/java/exercicioavaliativoextra/controller/NumeroPrimoController.java
@@ -1,10 +1,30 @@
 package exercicioavaliativoextra.controller;
 
 
+import exercicioavaliativoextra.service.NumeroPrimoService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("api/v1/primo")
 public class NumeroPrimoController {
+
+    NumeroPrimoService numeroPrimoService;
+
+    NumeroPrimoController(NumeroPrimoService numeroPrimoService){
+        this.numeroPrimoService = numeroPrimoService;
+    }
+    @GetMapping("/{valorNumero}")
+    public ResponseEntity<String> VerificarSeNumeroPrimo(@PathVariable("valorNumero") Integer valorNumero) {
+
+        boolean resultadoDaVerificacao = numeroPrimoService.VerificarSeNumeroPrimo(valorNumero);
+        if (resultadoDaVerificacao) {
+            return ResponseEntity.ok("Numero informado é primo");
+        } else {
+            return ResponseEntity.ok("Numero informado não é primo");
+        }
+    }
 }
--- Terceiro_Modulo/LinguagemProgramacao_III/ExerciciosAvaliativos/exercicio-avaliativo-extra/src/main/java/exercicioavaliativoextra/service/NumeroPrimoService.java
+++ Terceiro_Modulo/LinguagemProgramacao_III/ExerciciosAvaliativos/exercicio-avaliativo-extra/src/main/java/exercicioavaliativoextra/service/NumeroPrimoService.java
@@ -4,4 +4,17 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class NumeroPrimoService {
+
+    public boolean VerificarSeNumeroPrimo(Integer valorNumero) {
+
+        if (valorNumero == 1) {
+            return false;
+        } else {
+            for (int i = 2; i < valorNumero; i++) {
+                if (valorNumero % i == 0)
+                    return false;
+            }
+            return true;
+        }
+    }
 }
