6149bbcf0f67fe511afac6a9cd2b5b331cddf04a
mairyeduardo <mairyeduardo@gmail.com>
2023-10-11 22:14:49 -0300

    clean code finalizado

--- Terceiro_Modulo/LinguagemProgramacao_III/ExerciciosAvaliativos/exercicio-avaliativo-02/src/br/org/fundatec/service/ContratoService.java
+++ Terceiro_Modulo/LinguagemProgramacao_III/ExerciciosAvaliativos/exercicio-avaliativo-02/src/br/org/fundatec/service/ContratoService.java
@@ -26,47 +26,48 @@ public class ContratoService {
 
         Contrato contrato = null;
 
-        if (isNull(funcionario)) {
-            System.out.println("Não é possível criar um Contrato sem um funcionário");
-            return null;
-        } else if (isNull(cliente)) {
-            System.out.println("Não é possível criar um Contrato sem um cliente");
-            return null;
-        } else if (isNull(imovel)) {
-            System.out.println("Não é possível criar um Contrato sem um imovel");
-            return null;
-
-        } else if (isNull(imobiliaria)) {
-            System.out.println("Não é possível criar um Contrato sem uma imobiliaria");
-            return null;
-
-        } else {
-
+        if (isContratoPossuiDadosNecessarios(funcionario, cliente, imovel, imobiliaria)) {
             if (!isFuncionarioIgualCliente(funcionario, cliente)) {
                 if (isRendaClienteMaiorQueRendaMinima(cliente, funcionario, imovel, imobiliaria)) {
                     contrato = new Contrato(1L, funcionario, cliente, imovel, imobiliaria);
-                    System.out.println("=======================================================================");
                     System.out.println("Contrato criado com sucesso para o Imóvel: " + imovel.getIdImovel());
                     return contrato;
-                } else {
-                    System.out.println("=======================================================================");
-                    System.out.println("Não é possível criar um Contrato se a renda do cliente é menor que R$ " + RENDA_MINIMA);
-                    return null;
                 }
-
-            } else {
-                System.out.println("=======================================================================");
-                System.out.println("Não é possível criar um Contrato se a renda do cliente é menor que R$ " + RENDA_MINIMA);
-                return null;
             }
         }
+        return null;
     }
 
 
+    private boolean isContratoPossuiDadosNecessarios(Funcionario funcionario, Cliente cliente, Imovel imovel, Imobiliaria imobiliaria) {
+
+        if (isNull(funcionario)) {
+            System.out.println("==================================================");
+            System.out.println("Não é possível criar um Contrato sem um funcionário");
+            return false;
+        } else if (isNull(cliente)) {
+            System.out.println("==================================================");
+            System.out.println("Não é possível criar um Contrato sem um cliente");
+            return false;
+        } else if (isNull(imovel)) {
+            System.out.println("==================================================");
+            System.out.println("Não é possível criar um Contrato sem um imovel");
+            return false;
+
+        } else if (isNull(imobiliaria)) {
+            System.out.println("==================================================");
+            System.out.println("Não é possível criar um Contrato sem uma imobiliaria");
+            return false;
+        } else {
+            System.out.println("==================================================");
+            System.out.println("Contrato possui todos os campos preenchidos corretamente!");
+            return true;
+        }
+    }
+
     private boolean isFuncionarioIgualCliente(Funcionario funcionario, Cliente cliente) {
         if (funcionario.getNome().equals(cliente.getNome())) {
-            System.out.println("=======================================================================");
-            System.out.println("Um funcionario nao pode realizar uma venda para ele mesmo.");
+            System.out.println("Um funcionario não pode realizar uma venda para ele mesmo.");
             return true;
         }
         return false;
@@ -76,7 +77,9 @@ public class ContratoService {
         if (cliente.getRendaMensal().compareTo(RENDA_MINIMA) != -1) {
             System.out.println("Renda do Cliente é maior que a renda minima necessaria");
             return true;
+        } else {
+            System.out.println("Não é possível criar um Contrato se a renda do cliente é menor que R$ " + RENDA_MINIMA);
+            return false;
         }
-        return false;
     }
 }
