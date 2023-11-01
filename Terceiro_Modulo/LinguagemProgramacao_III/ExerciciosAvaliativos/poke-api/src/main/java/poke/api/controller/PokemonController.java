ff3f9fb7b4316258bb91e56f537bb789a6430659
mairyeduardo <mairyeduardo@gmail.com>
2023-10-31 22:06:41 -0300

    outros endpoints

--- Terceiro_Modulo/LinguagemProgramacao_III/ExerciciosAvaliativos/poke-api/src/main/java/poke/api/controller/PokemonController.java
+++ Terceiro_Modulo/LinguagemProgramacao_III/ExerciciosAvaliativos/poke-api/src/main/java/poke/api/controller/PokemonController.java
@@ -1,18 +1,17 @@
 package poke.api.controller;
 
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import poke.api.model.Pokemon;
 import poke.api.service.PokemonService;
 
 import java.util.List;
 
 @RestController
-@RequestMapping("api/v1/pokemon")
+@RequestMapping("api/v1/pokemons")
 public class PokemonController {
 
     private PokemonService pokemonService;
@@ -22,14 +21,36 @@ public class PokemonController {
     }
 
     @GetMapping
-    public ResponseEntity<List<Pokemon>> BuscarTodosPokemons() {
+    public ResponseEntity<List<Pokemon>> buscarTodosPokemons() {
         return ResponseEntity.ok(this.pokemonService.buscarTodos());
     }
 
     @GetMapping("/{nome}")
-    public ResponseEntity<Pokemon> BuscarPokemonPeloNome(@PathVariable("nome") String nome) {
+    public ResponseEntity<Pokemon> buscarPokemonPeloNome(@PathVariable("nome") String nome) {
         return ResponseEntity.ok(this.pokemonService.buscarPokemonPorNome(nome));
     }
 
+    @PostMapping()
+    public ResponseEntity<Void> adicionarPokemons(@RequestBody Pokemon pokemon) {
+        pokemonService.adicionar(pokemon);
+        return new ResponseEntity<>(null, HttpStatus.CREATED);
+    }
 
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Pokemon> removerPokemonsPorId(@PathVariable("id") Long id) {
+        Pokemon pokemonRemovido = pokemonService.removerPorId(id);
+        return ResponseEntity.ok(pokemonRemovido);
+    }
+
+    // Todo implementar endpoint para remover pokemon por nome
+    @DeleteMapping("/{nome}")
+    public ResponseEntity<Pokemon> removerPokemonPorNome(){
+        return null;
+    }
+
+    // Todo endpoint para alterar
+    @PutMapping("/{id}")
+    public ResponseEntity<Pokemon> alterarPokemonPorId(){
+        return null;
+    }
 }
--- Terceiro_Modulo/LinguagemProgramacao_III/ExerciciosAvaliativos/poke-api/src/main/java/poke/api/service/PokemonService.java
+++ Terceiro_Modulo/LinguagemProgramacao_III/ExerciciosAvaliativos/poke-api/src/main/java/poke/api/service/PokemonService.java
@@ -23,6 +23,13 @@ public class PokemonService {
         return pokemonRepository.findByNome(nome);
     }
 
+    public void adicionar(Pokemon pokemon) {
+        pokemonRepository.save(pokemon);
+    }
 
-
+    public Pokemon removerPorId(Long id){
+       Pokemon pokemonParaRemover = pokemonRepository.findById(id).get();
+       pokemonRepository.delete(pokemonParaRemover);
+       return pokemonParaRemover;
+    }
 }
